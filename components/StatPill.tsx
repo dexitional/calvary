707b5ef54cac4d66e8eacab8235251c839a0ef62
dexitional <ebenezerkb.ackah@gmail.com>
2023-05-22 15:37:22 +0000

    new fix

--- components/StatPill.tsx
+++ components/StatPill.tsx
@@ -12,7 +12,7 @@ function StatPill({ title,value, Icon }: Props) {
     <div className="pt-6 md:pt-0 h-full flex items-center space-x-4 ">
     <Icon className="h-10 w-10 md:h-14 md:w-14 text-white" />
         <div className="flex flex-col md:space-y-2 font-medium text-white">
-            <span className="text-lg md:text-3xl">{value}</span>
+            <span className="text-lg md:text-3xl text-slate-50/30 md:text-white">{value}</span>
             <span className="text-xs tracking-widest font-semibold">{title}</span>
         </div>
     </div>
