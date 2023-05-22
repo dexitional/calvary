17a5f9a0bdcea90d2ec006dc2def4071784a4521
dexitional <ebenezerkb.ackah@gmail.com>
2023-05-22 13:59:44 +0000

    new fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -26,11 +26,11 @@ async function Footer() {
   
   return (
     <footer className="w-full">
-      <section className="w-full md:h-96 bg-[#153B50E8] text-white">
+      <section className="w-full md:h-52 bg-[#020a75] text-white">
          <div className="py-6 px-3 md:mx-auto h-full w-full md:max-w-6xl md:grid md:grid-cols-4 md:gap-x-20 md:place-items-center">
              <div className="md:col-span-1 flex flex-col items-center justify-center space-y-4 md:space-y-10">
-                {/* <Image src={Logo} alt="" className="h-20 w-20 object-contain" /> */}
-                <h3 className="text-sm md:text-xl uppercase text-center font-serif">{setting.site_name}</h3>
+                <div className="px-10 rounded bg-white/70 relative h-24 w-56"><Image src={Logo} alt="" className="object-contain" fill /></div>
+                {/* <h3 className="text-sm md:text-xl uppercase text-center font-serif">{setting.site_name}</h3> */}
                 <p className="text-[0.75rem] text-center font-medium">{setting.site_desc}</p>
              </div>
              <div className="pt-6 col-span-2 md:w-4/5 hidden md:grid md:grid-cols-2 gap-24">
@@ -50,13 +50,11 @@ async function Footer() {
                       { title:'Home', link:'/faq'},
                       { title:'About Us', link:'/voting'},
                       { title:'Contact Us', link:'/opinion'},
-                      { title:'Events', link:'/events'},
-                      { title:'Press Releases', link:'/press'},
                      ]
                     } 
                  />
              </div>
-             <div className="pt-20 md:col-span-1 md:place-self-start">
+             <div className="pt-6 md:col-span-1 md:place-self-start">
                 <FootSocials heading="Connect With Us" links={{ facebook:setting.facebookUrl,twitter:setting.twitterUrl,instagram:setting.instagramUrl,youtube:setting.youtubeUrl}} />
              </div>
          </div>
--- components/Statistics.tsx
+++ components/Statistics.tsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import StatPill from './StatPill'
 import { FaRegFlag, FaVoteYea } from 'react-icons/fa'
-import { GiVote } from 'react-icons/gi'
+import { GiAudioCassette, GiVote } from 'react-icons/gi'
 import { BsPersonVcard } from 'react-icons/bs'
 import { BiCalendarEvent } from 'react-icons/bi'
+import { MdLocationOn } from 'react-icons/md'
 
 function Statistics() {
   return (
-    <div className="w-full bg-[#16c193]">
+    <div className="w-full bg-[#086ecd]">
         <div className="mx-auto px-6 h-24 w-full max-w-6xl grid grid-cols-4 gap-10 place-content-center">
             <StatPill title="SUNDAY SERVICE" value="8:30 AM" Icon={BiCalendarEvent} />
-            <StatPill title="VOTING STATIONS" value="33,000" Icon={GiVote} />
-            <StatPill title="POLITICAL PARTIES" value="33,000" Icon={FaRegFlag} />
-            <StatPill title="REGISTERED VOTERS" value="33,000" Icon={BsPersonVcard} />
+            <StatPill title="WEDNESDAY SERVICE" value="8:00 PM" Icon={BiCalendarEvent} />
+            <StatPill title="DAILY PODCAST" value="8:00 PM" Icon={GiAudioCassette} />
+            <StatPill title="WORSHIP DESTINATION" value="Daban - Ksi" Icon={MdLocationOn} />
         </div>
     </div>
   )
